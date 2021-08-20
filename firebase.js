c91308318ce6b0cc34c128384bf283b82229697f
Clay Bennington <claymation296@gmail.com>
2021-08-20 16:07:01 -0600

    Updated firebase to @next version. Removed old commented out code. Fixed bug with buildCompoundQueryParams function adding an undefined value in the returned array when orderBy was not being used in the job query. Fixed the cloudFunction import in services/services.js which was resolving as the module instead of the function.

--- firebase.js
+++ firebase.js
@@ -1,13 +1,10 @@
 
 
-// // This function is provided to 'app-shell/auth/auth.js' as a way
-// // to keep the firebase dependency isolated to this one repo,
-// // to make it easier to maintain. Otherwise there can be different 
-// // versions of firebase and it becomes a resolution nightmare.
-// const loadAuth = () => import(
-//   /* webpackChunkName: 'firebase/auth' */ 
-//   'firebase/auth'
-// );
+// This module is intended as a way to keep the firebase dependency 
+// isolated to this one repo, to make it easier to maintain. 
+//
+// Otherwise there can be different versions of firebase, and it 
+// becomes a resolution nightmare.
 
 
 let readyResolver;
@@ -15,39 +12,6 @@ let readyResolver;
 const ready = new Promise(resolve => {
   readyResolver = resolve;
 });
-
-
-// const loadFb = async () => {
-
-//   // Must use module resolution in webpack config and include config.js file in root
-//   // of src folder (ie. resolve: {modules: [path.resolve(__dirname, 'src'), 'node_modules'],}).
-//   const {firebaseConfig}    = await import(/* webpackChunkName: 'firebaseConfig' */ 'config.js');
-//   const {default: firebase} = await import(/* webpackChunkName: 'firebase/app' */ 'firebase/app');
-
-//   firebase.initializeApp(firebaseConfig);
-
-//   // Must fix 'IDBIndex undefined' error that causes
-//   // googlebot to not render on search console before 
-//   // including performance monitoring.
-//   // Adding the indexeddbshim for the legacy build 
-//   // in webpack does NOT solve this issue.
-//   await Promise.all([
-//     import(/* webpackChunkName: 'firebase/performance' */ 'firebase/performance'),
-//     import(/* webpackChunkName: 'firebase/analytics' */   'firebase/analytics')
-//   ]);
-
-//   // Initialize Firebase Analytics and Performance Monitoring.
-//   const performance = firebase.performance();
-//   const analytics   = firebase.analytics();
-
-//   readyResolver({
-//     analytics,
-//     firebase,
-//     loadAuth,
-//     performance
-//   });
-// };
-
 
 
 const loadFb = async () => {
@@ -65,10 +29,7 @@ const loadFb = async () => {
   const performance = getPerformance(firebaseApp);
   const analytics   = getAnalytics(firebaseApp);
   
-  // This function is provided to 'app-shell/auth/auth.js' as a way
-  // to keep the firebase dependency isolated to this one repo,
-  // to make it easier to maintain. Otherwise there can be different 
-  // versions of firebase and it becomes a resolution nightmare.
+  // This function is provided to 'app-shell/auth/auth.js'.
   const loadAuth = async () => {
 
     const {
