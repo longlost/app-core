1649b25ff26daefba9c3e0d1af6c81636b146652
Clay Bennington <claymation296@gmail.com>
2021-12-27 12:43:14 -0700

    Moved firebaseui dep into this repo to make dependency maintenance for firebase easier. Updated deps.

---
+++ firebaseui.js
@@ -0,0 +1,5 @@
+
+// These imports moved here to make dependency maintenance easier.
+
+export {default as styles} from 'firebaseui/dist/firebaseui.css';
+export * as firebaseui     from 'firebaseui';
