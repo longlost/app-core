167a3bf972c329b7c9ee7c337b04bad7826fd1ea
Clay Bennington <claymation296@gmail.com>
2022-05-25 18:02:44 -0600

    No using polymer's include syntax to properly import external style sheets. This is in preparation to an upgrade to Polymer 3.5 which supports and enforces Trusted Types.

--- firebaseui.js
+++ firebaseui.js
@@ -1,5 +1,9 @@
 
 // These imports moved here to make dependency maintenance easier.
 
-export {default as styles} from 'firebaseui/dist/firebaseui.css';
-export * as firebaseui     from 'firebaseui';
+// Importing here allows 'polymer-css-loader' to pick it up,
+// which exposes it to be included in the consuming modules' dom styles.
+//    ie. <style include="firebaseui">
+import 'firebaseui/dist/firebaseui.css';
+
+export * from 'firebaseui';
